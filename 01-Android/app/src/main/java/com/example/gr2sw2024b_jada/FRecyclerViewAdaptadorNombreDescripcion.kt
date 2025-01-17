19bf2429596fb53a01be58d8985f2bcc01f07903
Jorge Dueñas <jorge.duenas@epn.edu.ec>
2025-01-17 10:46:07 -0500

    Avance Clase

--- 01-Android/app/src/main/java/com/example/gr2sw2024b_jada/FRecyclerViewAdaptadorNombreDescripcion.kt
+++ 01-Android/app/src/main/java/com/example/gr2sw2024b_jada/FRecyclerViewAdaptadorNombreDescripcion.kt
@@ -1,4 +1,62 @@
 package com.example.gr2sw2024b_jada
 
-class FRecyclerViewAdaptadorNombreDescripcion {
+import android.view.LayoutInflater
+import android.view.TextureView
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class FRecyclerViewAdaptadorNombreDescripcion (
+    private val contexto: FRecyclerView,
+    private val lista: ArrayList<BEntrenador>,
+    private val recyclerView: RecyclerView
+): RecyclerView.Adapter<
+        FRecyclerViewAdaptadorNombreDescripcion.MyViewHolder>() {
+            inner class MyViewHolder(
+                view: View
+            ): RecyclerView.ViewHolder(view) {
+                var nombreTextView: TextView
+                val descripcionTextView: TextView
+                val likesTextView: TextView
+                val accionBoton: Button
+                var numeroLikes = 0
+                init {
+                    nombreTextView = view.findViewById(R.id.tv_nombre)
+                    descripcionTextView = view.findViewById(R.id.tv_descripcion)
+                    likesTextView = view.findViewById(R.id.tv_likes)
+                    accionBoton = view.findViewById(R.id.btn_dar_like)
+                    accionBoton.setOnClickListener { anadirLikes() }
+                }
+                fun anadirLikes() {
+                    numeroLikes = numeroLikes + 1
+                    likesTextView.text = numeroLikes.toString()
+                    contexto.aumentarTotalLikes()
+                }
+            }
+
+    // Layout que vamos a utilizar
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemView = LayoutInflater
+            .from(parent.context)
+            .inflate(
+                R.layout.recycler_view_vista,
+                parent,
+                false
+            )
+        return MyViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return lista.size
+    }
+
+    // Datos para la interacción
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val entrenadorActual = lista[position]
+        holder.nombreTextView.text = entrenadorActual.nombre
+        holder.descripcionTextView.text = entrenadorActual.descripcion
+        holder.likesTextView.text = holder.numeroLikes.toString()
+    }
 }
